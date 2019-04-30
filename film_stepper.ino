6c844f33ada770d2bc741be60c7c7e0d9c8d0c95
hmc-herring-s19 <ssmyth@cs.hmc.edu>
2019-04-29 17:09:11 -0700

    cleaned up variable declaration and added functions to determine steps and delays given distance, desired speed, pitch diameter

--- film_stepper.ino
+++ film_stepper.ino
@@ -1,12 +1,14 @@
-// testing a stepper motor with a Pololu A4988 driver board or equivalent
-// found: https://forum.arduino.cc/index.php?topic=506797.0
-
-// this version uses micros() to manage timing to allow high step rates to be tested
-// and illustrates a simple method for accleration and deceleration
-
+/*
+ * FILM lab
+ * Functionality for moving horizontal surge motor
+ * 
+*/
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+//#include <LiquidCrystal_I2C.h>
 
+/*
+ * Pin setup:
+ */
 byte directionPin = 9;
 byte stepPin = 8;
 byte enablePin = 10;
@@ -14,33 +16,41 @@ byte enablePin = 10;
 byte sdaPin = 4;
 byte sclPin = 5;
 
-// Predetermined times in microseconds
-unsigned long prevStepTime = 0;
+
+/*
+ * Motor Properties:
+ */
+int stepsPerRev = 200;  // = 360/step angle of motor
+byte direction = 1;
+
+/*
+ * Setup variables for simple movement
+ */
 // Delays between steps. For slower movements the delay is longer.
 unsigned long slowInterval = 1500;
-unsigned long fastInterval = 300;
-unsigned long stepInterval;
-unsigned long stepAdjustment;
-
-// Steps and revolutions
-int stepsPerRev = 200;
+unsigned long fastInterval = 300; // fastest is capped at 300
 // number of steps needed to accelerate
 int numAccelSteps = 100; // 100 is a half turn of a 200 step motor
 int revolutions = 20;
-
-int totalSteps = revolutions * stepsPerRev;
+unsigned long stepInterval;
+unsigned long stepAdjustment;
 int stepCounter;
-byte direction = 1;
+int totalSteps; 
 
-// Initialize LCD screen with I2C backpack.
-// Hardware initialized like so: LiquidCrystal_I2C lcdname(addr,en,rw,rs,d4,d5,d6,d7,bl,blpol);
+// counter for simple movement
+unsigned long prevStepTime = 0;
+
+/*
+ * Initialize LCD screen with I2C backpack
+ * 
+ * Hardware initialized like so: LiquidCrystal_I2C lcdname(addr,en,rw,rs,d4,d5,d6,d7,bl,blpol);
+ */
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, sdaPin, sclPin, 6, 7, 3, POSITIVE);
 
 
 void setup() {
 
     Serial.begin(115200);
-    Serial.println("Starting Stepper Demo with acceleration");
 
     // Set up pins
     pinMode(directionPin, OUTPUT);
@@ -48,17 +58,21 @@ void setup() {
     pinMode(enablePin, OUTPUT);
     
     digitalWrite(enablePin, LOW);
-    // If we want to accelerate, we need to slowly increase the step sizes over time.
+    /*
+     * Setup variables for move motor function
+     */
+    totalSteps = revolutions * stepsPerRev;
+    //totalSteps = getSteps(24.0, 0.891);
+    // step change if we're accelerating
     stepAdjustment = (slowInterval - fastInterval) / numAccelSteps;
     stepInterval = slowInterval;
     stepCounter = totalSteps;
     digitalWrite(directionPin, direction);
 
-    screenPrint("starting up", 500, 3);
+    screenPrint("Starting up", 500, 3);
 }
 
 void loop() {
-
     moveMotor();
 
 }
--- motor_functions.ino
+++ motor_functions.ino
@@ -1,12 +1,26 @@
-
-
+/*
+ * Function for simple movement with acceleration to reach higher speeds
+ * Parameters and how they affect function:
+ * slowInterval -> slower speed of motor
+ * fastInterval -> faster speed of motor
+ * numAccelSteps -> slower or faster acceleration
+ * revolutions -> how far the motor will go
+ * 
+ * For testing a stepper motor with a Pololu A4988 driver board or equivalent
+ * found: https://forum.arduino.cc/index.php?topic=506797.0
+ * This uses micros() to manage timing to allow high step rates to be tested, 
+ * uses a simple method for accleration and deceleration.
+ * Note: micros() has a resolution of 4 microseconds, and will always be a multiple of 4
+ * micros() will reset to 0 after 70 min of running the arduino (so it should stop running)
+ */
 void moveMotor() {
     // only move the designated # of steps
     if (stepCounter > 0) {
-        // we keep a count of the # microseconds that have gone by
-        // if # is greater then the delay we want, then we should move
-        // Note: micros() has a resolution of 4 microseconds, and will always be a multiple of 4
-        // micros() will reset to 0 after 70 min of running the arduino (so it should stop running)
+      /*
+       * prevStepTime = count of # microseconds that have gone by
+       * Only move if difference is > than the desired delay
+       * 
+       */
         if (micros() - prevStepTime >= stepInterval) {
             // increase the overall time count by the step interval
             prevStepTime += stepInterval;
@@ -48,3 +62,35 @@ void singleStep() {
     digitalWrite(stepPin, LOW);
 
 }
+
+/*
+ * Can be used to calculate the number of steps that should be input 
+ * in order to go a certain distance
+ * travelDist -> in inches
+ * pitchD -> in inches
+ * Has a cap distance to make sure we don't crash the carriage
+ */
+int getSteps(double travelDist, double pitchD) {
+    double maxDist = 30.0;
+    if (travelDist > maxDist) {
+      travelDist = maxDist;
+      Serial.println("WARNING: lower travelDist (currently capped)");
+    }
+    int revs = travelDist / (pitchD*3.14159);
+    int steps = revs * stepsPerRev;
+    return steps;
+}
+
+/*
+ * Used to determine the interval value (delay) required for specific
+ * speed of the motor
+ * metersPerSec -> speed
+ * travelDist -> in inches
+ */
+int getInterval(int steps, double metersPerSec, double travelDist) {
+    double ftPerSec = 3.28084*metersPerSec;
+    double timeSec = (travelDist/12) / ftPerSec;
+    int interval = (timeSec*1000000)/steps;
+    return interval;
+}
+
